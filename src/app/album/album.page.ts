77e082c8e65de7ceb642f25794621a3b0ffbd4f7
AashikaL <aashikaaashika1799@gmail.com>
2023-08-31 18:00:15 +0530

    update

--- src/app/album/album.page.ts
+++ src/app/album/album.page.ts
@@ -1,27 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import albums from '../../assets/mockdata/albums';
+import albums from '../../assets/mockdata/albums/index';
 
 @Component({
   selector: 'app-album',
   templateUrl: './album.page.html',
   styleUrls: ['./album.page.scss'],
 })
-export class AlbumPage{
-  data = albums;
+export class AlbumPage {
+  data: any = albums; constructor(private activatedRoute: ActivatedRoute) { } 
+  ngOnInit() { console.log(this.data) ;   
+    const title = this.activatedRoute.snapshot.paramMap.get('title'); 
+    if (title) { const decodedTitle = decodeURIComponent(title); 
+      this.data = this.data[title]; } 
+    }
 
-  constructor(private activatedRoute: ActivatedRoute) { }
-
-  ngOnInit() {
-    const title:any = this.activatedRoute.snapshot.paramMap.get('title');
-    const decodedTitle = decodeURIComponent(title);
-    this.data = albums;    
-  }
-
-    // Helper function for image names
-    dasherize(string:any) {
-      return string.replace(/[A-Z]/g, function(char:any, index:any) {
-        return (index !== 0 ? '-' : '') + char.toLowerCase();
-      });
-    };
+  // Helper function for image names
+  dasherize(string: any) {
+    return string.replace(/[A-Z]/g, function (char: any, index: any) {
+      return (index !== 0 ? '-' : '') + char.toLowerCase();
+    });
+  };
 }
